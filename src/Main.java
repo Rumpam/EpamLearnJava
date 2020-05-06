46f5f603840618469b2f6f14d2227df3fb03bd1e
Rusak <rusa123@mail.ru>
2020-05-06 19:18:53 +0300

    second commit false merge

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
         int a = 1;
+        double d = 1.0;
+        float f = 1.0f;
         ++a;
         System.out.println(a);
     }
