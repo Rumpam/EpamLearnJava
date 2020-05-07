26be300d08d0a779a5cb8e5aac5bd104c91a5f49
Rusak <rusa123@mail.ru>
2020-05-07 18:20:40 +0300

    change need task

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        int a = 1;
-        double d = 1.0;
-        float f = 1.0f;
-        ++a;
-        System.out.println(a);
-        System.out.println(d);
-        //change file in web. After false pull in PC
-        //after stash apply
+        String hello = "Hello";
+        String world = "world";
+		System.out.println(hello + " " + world + "!");
     }
 }
